e4cfb19b711ea5cfffe883eb23057f901f2f76f7
shashankshuklacodes <shashank.shukla1908@gmail.com>
2025-06-23 20:33:36 +0530

    Connected frontend to live backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-   baseURL: 'https://your-backend-name.onrender.com/api',
+   baseURL: 'https://ecommerce-backend-ah0o.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
